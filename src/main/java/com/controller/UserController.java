14349c317254652f6da5164a63971345ba59ad1e
Administrator <1458127569@qq.com>
2019-08-26 09:52:01 +0800

    Initial commit

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.controller;
+
+import com.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/getUserByPhone")
+    @ResponseBody
+    public String getUserByPhone() {
+        return "123456";
+    }
+}
---
+++ src/main/java/com/service/UserService.java
@@ -0,0 +1,9 @@
+package com.service;
+
+import com.pojo.User;
+
+public interface UserService {
+
+    User getUserByPhone(String userPhone);
+
+}
